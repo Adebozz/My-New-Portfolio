c1c76f5e66c29e9b154633ca7f3ce2e56d0febd2
Adebozz <ademolaadeigbe3@gmail.com>
2023-03-30 05:52:08 +0100

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
     fontFamily: {
       signature: ['Great Vibes'],
-    }
+    },
   },
   plugins: [],
-}
+};
 
